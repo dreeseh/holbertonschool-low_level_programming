ed9521182eb5b54115cb7ed0a57e273085811b69
dreeseh <reese@konvertink.com>
2020-06-10 23:34:37 +0000

    complete redo of 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * Return: Zero
+ *
+ * Return: Always 0
+ *
  */
 int main(void)
 {
-puts("\"Programming is like building a multilinugal puzzle\n");
+  puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
