165e3de705a91ba49ff7e85cfe85933bed74e79c
dreeseh <reese@konvertink.com>
2020-06-10 22:13:53 +0000

    spaces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 *main - Entry point
 *Return: Always 0
@@ -9,6 +8,5 @@ int main(void)
 {
 
 puts("\"Programming is like building a multilinugal puzzle");
-
 return (0);
 }
