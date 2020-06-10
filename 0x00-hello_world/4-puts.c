4e5ae5739740ab6101442d59167a75a1c96276ba
dreeseh <reese@konvertink.com>
2020-06-10 21:48:11 +0000

    fixes betty issues

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-/*
-*main - Entry poinit
+/**
+*main - Entry point
+*Return: Always 0
 *This script prints the string without the use of printf
 */
 
 int main(void)
 {
-char str[] = "Programming is like building a multilinugal puzzle";
+char str[] = "\"Programming is like building a multilinugal puzzle";
 puts(str);
 
 return (0);
