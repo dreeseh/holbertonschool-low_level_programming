5ee7837d01c260b5155c526f67ebdf211d38e71e
dreeseh <reese@konvertink.com>
2020-06-10 21:06:43 +0000

    4 fixed for betty-style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 
-int main(){
-  char str[] = "Programming is like building a multilinugal puzzle";
-  puts (str);
-  return 0;
+int main(void)
+{
+char str[] = "Programming is like building a multilinugal puzzle";
+puts(str);
+
+return (0);
 }
