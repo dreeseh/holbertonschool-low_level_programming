33f696c7c535442eb7e1f150fdb94e8d52d30c5a
dreeseh <reese@konvertink.com>
2020-06-12 01:42:34 +0000

    n changed to o

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ int o;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 o = (n % 10);
-if (n == 0)
+if (o == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, o);
 }
-if (n < 6 && n != 0)
+if (o < 6 && o != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, o);
 }
-if (n > 5)
+if (o > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, o);
 }
