88e32e2fb56985b83122f5e6d3daa7960bc0a1e5
dreeseh <reese@konvertink.com>
2020-06-13 15:47:23 +0000

    adds c script 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - uses function getchar to print the alphabet
+ * Return: always zero
+ */
+
+int main(void)
+{
+int n;
+for (n = 1 ; n < 10 ; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
