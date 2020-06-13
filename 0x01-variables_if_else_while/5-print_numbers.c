8ae32490dd4f0d3097c028368518a4eb731be979
dreeseh <reese@konvertink.com>
2020-06-13 16:24:28 +0000

    5-print_numbers took out curlys

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,14 +3,11 @@
  * main - uses function getchar to print the alphabet
  * Return: always zero
  */
-
 int main(void)
 {
 int n;
 for (n = 1 ; n <= 9 ; n++)
-{
 printf("%d", n);
-}
 printf("\n");
 return (0);
 }
