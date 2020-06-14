70a8f656ee26ce0b7d4b2977833739f710da653c
dreeseh <reese@konvertink.com>
2020-06-14 20:57:19 +0000

    adds script 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - void
+ * Return: zero
+ */
+int main(void)
+{
+int n;
+int d;
+n = 0;
+while (n <= 99)
+{
+d = n / 10;
+putchar ('0' + d);
+d = n % 10;
+putchar('0' + d);
+if (n != 99)
+{
+putchar (',');
+putchar (' ');
+}
+else
+{
+putchar('\n');
+}
+n = n + 1;
+}
+return (0);
+}
