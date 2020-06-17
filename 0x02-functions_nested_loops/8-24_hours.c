93752d65c95e26653dd45b2f075a12b6d7ab2f1a
dreeseh <reese@konvertink.com>
2020-06-17 19:22:38 +0000

    adds all files for 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * jack_bauer - prints every minute of every day
+ *
+ * Return: always zero
+ */
+
+void jack_bauer(void)
+{
+  int hrs;
+  int min;
+  for (hrs = 0 ; hrs <= 24 ; hrs++)
+    {
+      for (min = 0 ; min <= 59 ; min++)
+	{
+	  _putchar('0' + hrs / 10);
+	  _putchar('0' + hrs % 10);
+	  _putchar(':');
+	  _putchar('0' + min / 10);
+	  _putchar('0' + min % 10);
+	  _putchar('\n');
+}
+}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -8,3 +8,9 @@ void print_alphabet(void);
 
 /* print alphabet 10x */
 void print_alphabet_x10(void);
+
+/* is lower function */
+int _islower(int c);
+
+/* prints every hour of every day */
+void jack_bauer(void);
