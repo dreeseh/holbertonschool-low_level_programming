f66a6e57b5c7431e62bc360e634b7e1c5649c26b
dreeseh <reese@konvertink.com>
2020-06-18 03:36:56 +0000

    everything needed for 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,18 +7,18 @@
 
 void jack_bauer(void)
 {
-  int hrs;
-  int min;
-  for (hrs = 0 ; hrs <= 24 ; hrs++)
-    {
-      for (min = 0 ; min <= 59 ; min++)
-	{
-	  _putchar('0' + hrs / 10);
-	  _putchar('0' + hrs % 10);
-	  _putchar(':');
-	  _putchar('0' + min / 10);
-	  _putchar('0' + min % 10);
-	  _putchar('\n');
+int hrs;
+int min;
+for (hrs = 0 ; hrs <= 24 ; hrs++)
+{
+for (min = 0 ; min <= 59 ; min++)
+{
+_putchar('0' + hrs / 10);
+_putchar('0' + hrs % 10);
+_putchar(':');
+_putchar('0' + min / 10);
+_putchar('0' + min % 10);
+_putchar('\n');
 }
 }
 }
