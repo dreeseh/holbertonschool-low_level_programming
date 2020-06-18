22394a1db1ca00274bb4da5ca540c14764151772
dreeseh <reese@konvertink.com>
2020-06-18 03:30:55 +0000

    redo comments for 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * print_last_digit - prints the last digit of a given input.
- * @int - given int from main.
+ * @n: given int from main.
  * Return: the last digit.
  */
 
