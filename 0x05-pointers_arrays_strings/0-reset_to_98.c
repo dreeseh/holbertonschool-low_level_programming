f3ef6337a4c18e3f07b1f67d04013e38023dc3e3
dreeseh <reese@konvertink.com>
2020-06-22 23:39:11 +0000

    everything for task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - resets n to 98
+ * @n: pointer that is reset to 98
+ * Return: nada
+ * function that takes to an int and updates it
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,52 @@
+/* Header file for holberton school */
+
+/* putchar */
+int _putchar(char c);
+
+/* print alphabet */
+void print_alphabet(void);
+
+/* print alphabet 10x */
+void print_alphabet_x10(void);
+
+/* is lower function */
+int _islower(int c);
+
+/* prints every hour of every day */
+void jack_bauer(void);
+
+/* prints the last digit */
+int print_last_digit(int);
+
+/* prints the alphabet 2x */
+int _isalpha(int c);
+
+/* prints the sign of a number */
+int print_sign(int n);
+
+/* prints the absolute value of a number */
+int _abs(int);
+
+/* determines if the function is uppercase or not */
+int _isupper(int c);
+
+/* checks for a digit 0 - 9 */
+int _isdigit(int c);
+
+/* multiplies two integers */
+int mul(int a, int b);
+
+/* prints numbers 0-9 */
+void print_numbers(void);
+
+/* prints 0-9 except for 2 & 4  */
+void print_most_numbers(void);
+
+/* prints the numbers 0 -14 ten times  */
+void more_numbers(void);
+
+/* prints a triangle  */
+void print_triangle(int size);
+
+/* resets pointer to 98 */
+void reset_to_98(int *n);
