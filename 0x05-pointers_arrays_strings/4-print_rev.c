a64834a93bcacef84566369f79a402646eb99eef
dreeseh <reese@konvertink.com>
2020-06-23 20:28:25 +0000

    everything needed for task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: char for sentence we're reversing
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int ticker = 0;
+
+while (s[ticker] != '\0')
+{
+	ticker++;
+}
+while (ticker > 0)
+{
+	_putchar(s[ticker]);
+	ticker--;
+}
+_putchar('\n');
+}
