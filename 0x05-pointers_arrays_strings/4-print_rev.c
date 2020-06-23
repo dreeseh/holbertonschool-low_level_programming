46a8af22de884cd981d6af9587a227d2eed3347a
dreeseh <reese@konvertink.com>
2020-06-23 20:46:13 +0000

    reformatted code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,16 @@ void print_rev(char *s)
 {
 	int ticker = 0;
 
-while (s[ticker] != '\0')
-{
-	ticker++;
-}
-while (ticker >= 0)
-{
-	_putchar(s[ticker]);
-	ticker--;
-}
+	while (s[ticker] != '\0')
+	{
+		ticker++;
+	}
+
+	while (ticker >= 0)
+	{
+		_putchar(s[ticker]);
+		ticker--;
+	}
+
 _putchar('\n');
 }
