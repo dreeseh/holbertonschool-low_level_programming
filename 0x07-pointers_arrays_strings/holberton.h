206260d2439ed6a9a8c5af97edb4bcbe22e3af1a
dreeseh <reese@konvertink.com>
2020-06-30 03:35:10 +0000

    everything for task 2

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -83,3 +83,6 @@ char *_memset(char *s, char b, unsigned int n);
 
 /* function that copies memory area */
 char *_memcpy(char *dest, char *src, unsigned int n);
+
+/* locates a char in a string  */
+char *_strchr(char *s, char c);
