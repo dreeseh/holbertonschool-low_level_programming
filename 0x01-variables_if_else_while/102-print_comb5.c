4b1d6b2e378e9133710fe7a7862b79ecb0348ad9
dreeseh <reese@konvertink.com>
2020-07-08 16:11:25 +0000

    betty fixes for advanced task #13

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,28 +2,28 @@
 #include <stdio.h>
 #include <time.h>
 /**
- * main - Entry point
+ * main - int
  *
- * Return: Always 0 (Success)
+ * Return: is zero
  */
 int main(void)
 {
-	int i, a, b, c;
+	int d, a, b, c;
 
-	for (i = 48; i <= 57; i++)
+	for (d = 48; d <= 57; d++)
 	{
 		for (a = 48; a <= 56; a++)
 		{
-			for (b = i; b <= 57; b++)
+			for (b = d; b <= 57; b++)
 			{
 				for (c = 48; c <= 57; c++)
 				{
-					putchar(i);
+					putchar(d);
 					putchar(a);
 					putchar(32);
 					putchar(b);
 
-if (i == b)
+if (d == b)
 					{
 if (c <= a)
 {
@@ -32,7 +32,7 @@ c = a + 1;
 					}
 
 					putchar(c);
-					if (i == 57 && a == 56 && b == 57 && c == 57)
+					if (d == 57 && a == 56 && b == 57 && c == 57)
 					{
 						putchar(10);
 					}
