393ad23ac00cccb2d65d3d04721643533e88a723
dreeseh <reese@konvertink.com>
2020-07-08 20:07:46 +0000

    corrects advanced task to x/10

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,15 +7,20 @@
 
 void print_number(int n)
 {
+	unsigned int x;
+
 	if (n < 0)
 	{
-		n = -n;
-		_putchar('-');
+		_putchar ('-');
+		x = -n;
 	}
-
-	if (n / 10)
+	else
 	{
-		print_number(n / 10);
-		_putchar(n % 10 + '0');
+		x = n;
 	}
+	if (x / 10 != 0)
+	{
+		print_number(x / 10);
+	}
+	_putchar((x % 10) + '0');
 }
