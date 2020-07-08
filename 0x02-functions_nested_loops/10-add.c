7800d372fa3f65b5d4a17d6b73246bebdf557b2d
dreeseh <reese@konvertink.com>
2020-07-08 16:55:27 +0000

    task 10 code

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @n1: number 1
+ * @n2: number 2
+ * Return: the sum of 1 & 2
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
