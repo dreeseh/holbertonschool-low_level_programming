6132d45e5c33891c42d2c8484ff7899bfd0a250f
dreeseh <reese@konvertink.com>
2020-07-13 22:47:21 +0000

    task two

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - prints the name from which the program was comiled
+ * @argc: integer type of arguments; contains total number of arguments
+ * @argv: array of character pointers; holds all the parameters
+ * Return: is zero
+ */
 int main(int argc, char *argv[])
 {
 	argc++;
