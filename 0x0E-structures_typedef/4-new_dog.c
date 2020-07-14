3ae55ff709682789e386760ee9f5bced63804d6f
dreeseh <reese@konvertink.com>
2020-07-14 23:43:40 +0000

    simplified task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
-
+#include <string.h>
 /**
  *new_dog - creates a new dog
  *@name: name of dog
@@ -12,13 +12,29 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog_t *new_dog;
+	dog_t *new_dog;
+	char *namelength, *ownerlength;
 
-	new_dog = malloc(sizeof(struct dog));
+	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
+	{
 		return (NULL);
-	new_dog->name = name;
+	}
+
+	namelength = malloc(sizeof(char) * strlen(name) + 1);
+	if (namelength == NULL)
+	{
+		return (NULL);
+	}
+	strcat(namelength, name);
+	ownerlength = malloc (sizeof(char) * strlen(owner) + 1);
+	if (ownerlength == NULL)
+	{
+		return (NULL);
+	}
+	strcat(ownerlength, owner);
+	new_dog->name = namelength;
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = ownerlength;
 	return (new_dog);
 }
