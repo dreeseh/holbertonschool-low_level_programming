b65f358e04094215bde0d78c1268d72e86354bb5
dreeseh <reese@konvertink.com>
2020-07-14 03:22:43 +0000

    betty fixes for header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef REESES_HEADER_H
 #define REESES_HEADER_H
+/**
+ * struct dog - structure that has 2 chars and one float
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owners name
+ */
+
 struct dog
 {
 	char *name;
