1be15caef57dbe86eb235fe8580971366833070a
dreeseh <reese@konvertink.com>
2020-07-14 04:32:45 +0000

    replaced  on nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,24 +11,14 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
 		return;
-	}
-
 	if (d->name == NULL)
-	{
-		d->name = "(nil)";
-	}
-
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
-	{
-		d->owner = "(nil)";
-	}
-
-	if (d->age == NULL)
-	{
-		d->age = "(nil)";
-	}
-
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
