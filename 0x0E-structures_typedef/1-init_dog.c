1843e0ad3fe44704c9218e5d1936b50a27ba1498
dreeseh <reese@konvertink.com>
2020-07-14 04:03:34 +0000

    betty fixes for task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include "dog.h"
-#include "holberton.h"
+
 /**
  * init_dog - initializes a structure of type dog
- * @d: pointer to structure
- * @name: pointer to the name of the dog
- * @age: age of dog
- * @owner: pointer to owner of dog
- *
- * Return: void
+ * @d: pointer to dog
+ * @name: pointer to name
+ * @age: age
+ * @owner: pointer to owner
+ * Return: is void
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
