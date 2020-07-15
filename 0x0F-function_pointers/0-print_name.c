739fa9825a024a6507db5e60361c764d7f5f5a9a
dreeseh <reese@konvertink.com>
2020-07-15 21:03:56 +0000

    header file, taks 0-2 & _putchar

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: function pointer
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL || name == NULL)
+		{
+			return (NULL);
+		}
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+index of 0x0F-function_pointers
