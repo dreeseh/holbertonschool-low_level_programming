b850bd90a4fb9fba5a9afa96a66e9651b0adcc76
dreeseh <reese@konvertink.com>
2020-07-16 03:04:25 +0000

    adds 4 files of task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,19 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - passing arguments to main
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: is zero
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return(98);
+	}
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds a and b
+ * @a: int 1
+ * @b: int 2
+ * Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts a from b
+ * @a: int 1
+ * @b: int 2
+ * Return: a minus b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies a and b
+ * @a: int 1
+ * @b: int 2
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides a into b
+ * @a: int 1
+ * @b: int 2
+ * Return: a divided by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulos a & b
+ * @a: int 1
+ * @b: int 2
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
