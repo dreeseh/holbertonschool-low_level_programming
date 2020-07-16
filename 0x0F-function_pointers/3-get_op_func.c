8818244d08d9327098db4da88e3a215b01de4cad
dreeseh <reese@konvertink.com>
2020-07-16 03:11:38 +0000

    betty fixes

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,11 +1,9 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "string.h"
 /**
- * get_op_func - selects the correct funtion to perform opperation
+ * get_op_func - function
  * @s: operator passed as argument to the program
- * @get_op_func: returns a pointer to the correct function
  * Return: pointer to the correct function
  */
 int (*get_op_func(char *s))(int, int)
