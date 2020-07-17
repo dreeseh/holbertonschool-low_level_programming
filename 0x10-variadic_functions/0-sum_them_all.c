a2647e043b8d0ff9d204f59fdaa955ad584a3435
dreeseh <reese@konvertink.com>
2020-07-17 20:41:12 +0000

    dir 0x10-variadic functions, readme, headerfile, task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - adds all list items
+ * @n: number of items in list
+ * Return: the sum of all items in list
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list my_list;
+	unsigned int counter;
+	int sum;
+
+	/* initialize list */
+	va_start (my_list, n);
+
+	/* step through list */
+	sum = 0;
+	if (n == 0)
+	{
+		return (0);
+	}
+	for (counter = 0 ; counter < n ; counter ++)
+	{
+		/* get next list item */
+		sum += va_arg (my_list, int);
+	}
+
+	/* clean memory */
+	va_end (my_list);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+index of 0x10-variadic_functions
