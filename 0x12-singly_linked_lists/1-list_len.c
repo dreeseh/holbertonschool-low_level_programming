0b41ef3afff62143f86accd7f638066be6c693c7
dreeseh <1859@holbertonschool.com>
2020-07-24 01:52:08 +0000

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len - returns # of nodes in a linked list
+ * @h: linked list pointer
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
