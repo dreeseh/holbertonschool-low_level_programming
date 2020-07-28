a81de57ae4b27df4dde6675f2bc2b0921331a8f9
dreeseh <1859@holbertonschool.com>
2020-07-28 14:55:43 +0000

    betty fixes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,9 +11,9 @@ void free_listint2(listint_t **head)
 	listint_t *holder;
 
 	if (head == NULL)
-        {
-                return;
-        }
+	{
+		return;
+	}
 	while (*head)
 	{
 		holder = *head;
