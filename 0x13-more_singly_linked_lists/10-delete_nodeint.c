81cfeba461d989fc8179c604c515b3c0bafb0022
dreeseh <1859@holbertonschool.com>
2020-07-28 22:17:50 +0000

    task 10 with betty fixes

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - deletes a node at index
+ * @head: pointer to head of list
+ * @index: point to delete node
+ * Return: 1 if succeded, -1 if failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *following;
+	unsigned int step;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	temp = *head;
+	if (index == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return (0);
+	}
+	for (step = 0 ; step < index - 1 ; step++)
+	{
+		temp = temp->next;
+	}
+	if (temp == NULL || temp->next == NULL)
+	{
+		return (0);
+	}
+	following = temp->next->next;
+	free(temp->next);
+	temp->next = following;
+	return (1);
+}
