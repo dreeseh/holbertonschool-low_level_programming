7812f100bfe0ce13575b74f3c69186d6d87bd935
dreeseh <1859@holbertonschool.com>
2020-07-28 15:58:03 +0000

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deleted the head node of listint_t
+ * @head: double pointer to head of list
+ * Return: data from head node
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	n = (*head)->n;
+	temp = *head;
+	*head = temp->next;
+	free(temp);
+	return(n);
+}
