89ea05f681effadadec0b3ce89c77a5f9befbd6d
dreeseh <1859@holbertonschool.com>
2020-08-21 14:21:45 +0000

    advanced task one

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_number - prints an integer
+ * @n: int to print
+ */
+void print_number(int n)
+{
+	unsigned int i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -n;
+	}
+	else
+		i = n;
+
+	if (i / 10)
+		print_number(i / 10);
+
+	_putchar(i % 10 + '0');
+}
