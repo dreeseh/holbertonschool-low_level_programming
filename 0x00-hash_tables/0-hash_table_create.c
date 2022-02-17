bc1a2b41cd56d459a3497fd34c647e1a73eb063b
dreeseh <1859@holbertonschool.com>
2022-02-17 19:12:43 +0000

    changed ==NULL to !

--- 0x00-hash_tables/0-hash_table_create.c
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	new_hash = malloc(sizeof(hash_table_t));
-	if (new_hash == NULL)
+	if (!new_hash)
 	{
 		return (NULL);
 	}
@@ -23,7 +23,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_hash->size = size;
 
 	new_hash->array = malloc(sizeof(hash_node_t *) * size);
-	if (new_hash->array == NULL)
+	if (!new_hash->array)
 	{
 		free(new_hash);
 		return (NULL);
