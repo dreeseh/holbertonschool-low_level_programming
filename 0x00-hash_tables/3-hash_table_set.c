0a00f78a8c5059945c1a73ba8f6bb66401d597b4
dreeseh <1859@holbertonschool.com>
2022-02-17 18:14:58 +0000

    betty fix

--- 0x00-hash_tables/3-hash_table_set.c
+++ 0x00-hash_tables/3-hash_table_set.c
@@ -1,14 +1,14 @@
 #include "hash_tables.h"
-
-/*
+/**
  * hash_table_set - a function that adds an element to the hash table
+ * @ht: the hash table you want to add or update the key/value to
  * @key: is the key. key can not be an empty string
- * @value: is the value associated with the key. value must be duplicated. value can be an empty string
+ * @value: is the value associated with the key
  * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-  int index;
+	int index;
 	hash_node_t *new_node, *current_node;
 
 	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
