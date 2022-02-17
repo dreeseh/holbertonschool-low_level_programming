5c60ef2e389d9cca3de69509935cc6fcb7b1a8f7
dreeseh <1859@holbertonschool.com>
2022-02-17 18:01:32 +0000

    tabs fix 2

--- 0x00-hash_tables/0-hash_table_create.c
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 /**
  * hash_table_create - a function that creates a hash table
- * @size: size of the array
- * Return: a new hash table
+ * @size: the size of the array
+ * Return: a pointer to the newly created hash table or NULL
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
