0efff8dd0eb36e082d5541432f3b5a8934269650
dreeseh <1859@holbertonschool.com>
2022-02-17 18:35:53 +0000

    betty fix

--- 0x00-hash_tables/4-hash_table_get.c
+++ 0x00-hash_tables/4-hash_table_get.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 
-/*
+/**
  * hash_table_get -  a function that retrieves a value associated with a key
  * @ht: is the hash table you want to look into
  * @key: is the key you are looking for
@@ -8,7 +8,7 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-        int index;
+	int index;
 	hash_node_t *new;
 
 	if (ht == NULL || key == NULL)
