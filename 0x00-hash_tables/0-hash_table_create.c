195db6ff61dfc60fe0912a62fc6eaf665f903288
dreeseh <1859@holbertonschool.com>
2022-02-17 18:16:28 +0000

    removed spaces

--- 0x00-hash_tables/0-hash_table_create.c
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -1,11 +1,9 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_create - a function that creates a hash table
  * @size: the size of the array
  * Return: a pointer to the newly created hash table or NULL
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash;
