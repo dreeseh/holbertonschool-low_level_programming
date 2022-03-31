764d5de788084a4e2fc84d6ca8a8a25a40b77f13
dreeseh <1859@holbertonschool.com>
2022-03-31 14:28:52 +0000

    header file & 106 c lang files for mando tasks

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - a search algo with an express lane
+ * @list: list of numbers to be searched
+ * @value: the value to find in the list
+ * Return: a pointer on the first node where value is located
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *tail = list;
+
+	if (!list)
+		return (NULL);
+	
+	while (tail->express && value > tail->n)
+	{
+		list = tail;
+		tail = tail->express;
+		printf("Value checked at index [%lu] = [%d]\n", tail->index, tail->n);
+	}
+	if (value > tail->n)
+	{
+		list = tail;
+		while (tail->next)
+			tail = tail->next;
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n",
+			list->index, tail->index);
+	tail = tail->next;
+	while (list != tail)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+		if (list->n == value)
+			return (list);
+		list = list->next;
+	}	
+	return (NULL);
+}
