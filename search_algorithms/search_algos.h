3809b9c94bda9aa5b010e9a8019fa0ac42d28029
dreeseh <1859@holbertonschool.com>
2023-05-10 15:44:16 +0000

    added header file

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Prototypes */
+int interpolation_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
